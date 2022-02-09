e54e89fe6d4d9ee349f6d6c13c9d9c18a8429e63
JOSUA BRINK - STUDENT <G00362643@gmit.ie>
2022-02-09 10:15:37 +0000

    Phone Added

--- src/test/java/ie/gmit/PassengerTest.java
+++ src/test/java/ie/gmit/PassengerTest.java
@@ -9,11 +9,11 @@ public class PassengerTest {
     @Test
     void testTitle()
     {
-        p = new Passenger("Mr", "Stevens", "0123456789");
+        p = new Passenger("Mr", "Stevens", "0123456789", "01234567");
         assertEquals("Mr", p.getTitle());
-        p = new Passenger("Mrs", "Stevens", "0123456789");
+        p = new Passenger("Mrs", "Stevens", "0123456789", "01234567");
         assertEquals("Mrs", p.getTitle());
-        p = new Passenger("Ms", "Stevens", "0123456789");
+        p = new Passenger("Ms", "Stevens", "0123456789", "01234567");
         assertEquals("Ms", p.getTitle());
     }
 
@@ -23,7 +23,7 @@ public class PassengerTest {
         final String invalid = "Invalid title entered";
         Exception exceptionThrown = assertThrows(
                 IllegalArgumentException.class,
-                ()-> new Passenger("Miss", "Stevens", "0123456789")
+                ()-> new Passenger("Miss", "Stevens", "0123456789", "01234567")
         );
 
         assertEquals(invalid, exceptionThrown.getMessage());
@@ -32,7 +32,7 @@ public class PassengerTest {
     @Test
     void testName()
     {
-        p = new Passenger("Mr", "Stevens", "0123456789");
+        p = new Passenger("Mr", "Stevens", "0123456789", "01234567");
         assertEquals("Stevens", p.getName());
     }
 
@@ -42,7 +42,7 @@ public class PassengerTest {
         final String invalid = "Invalid name entered";
         Exception exceptionThrown = assertThrows(
                 IllegalArgumentException.class,
-                ()-> new Passenger("Ms", "1", "0123456789")
+                ()-> new Passenger("Ms", "1", "0123456789", "01234567")
         );
 
         assertEquals(invalid, exceptionThrown.getMessage());
@@ -51,7 +51,7 @@ public class PassengerTest {
     @Test
     void testID()
     {
-        p = new Passenger("Mr", "Stevens", "0123456789");
+        p = new Passenger("Mr", "Stevens", "0123456789", "01234567");
         assertEquals("0123456789", p.getID());
     }
 
@@ -61,7 +61,25 @@ public class PassengerTest {
         final String invalid = "Invalid ID entered";
         Exception exceptionThrown = assertThrows(
                 IllegalArgumentException.class,
-                ()-> new Passenger("Ms", "Jones", "1")
+                ()-> new Passenger("Ms", "Jones", "1", "01234567")
+        );
+        assertEquals(invalid, exceptionThrown.getMessage());
+    }
+
+    @Test
+    void testPhone()
+    {
+        p = new Passenger("Mr", "Stevens", "0123456789", "01234567");
+        assertEquals(p.getPhone(), "01234567");
+    }
+
+    @Test
+    void testInvalidPhone()
+    {
+        final String invalid = "Invalid Phone Number entered";
+        Exception exceptionThrown = assertThrows(
+                IllegalArgumentException.class,
+                ()-> new Passenger("Ms", "Jones", "0123456789", "1")
         );
         assertEquals(invalid, exceptionThrown.getMessage());
     }
