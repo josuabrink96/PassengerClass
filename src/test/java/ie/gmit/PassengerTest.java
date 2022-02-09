aebc3c46bc7fca45b19c9343acaa2b5ed5140112
JOSUA BRINK - STUDENT <G00362643@gmit.ie>
2022-02-09 09:35:31 +0000

    titleAdded

--- src/test/java/ie/gmit/PassengerTest.java
+++ src/test/java/ie/gmit/PassengerTest.java
@@ -1,5 +1,32 @@
 package ie.gmit;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PassengerTest {
+    Passenger p;
+
+    @Test
+    void testTitle()
+    {
+        p = new Passenger("Mr");
+        assertEquals("Mr", p.getTitle());
+        p = new Passenger("Mrs");
+        assertEquals("Mrs", p.getTitle());
+        p = new Passenger("Ms");
+        assertEquals("Ms", p.getTitle());
+    }
+
+    @Test
+    void testInvalidTitle()
+    {
+        final String invalid = "Invalid title entered";
+        Exception exceptionThrown = assertThrows(
+                IllegalArgumentException.class,
+                ()-> new Passenger("Miss"));
+
+        assertEquals(invalid, exceptionThrown.getMessage());
+    }
+
 
 }
